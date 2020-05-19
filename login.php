00ccd03d89e40d9ecf8c361101733d1008aca563
HelloDearSir <jonbarton@hotmail.co.uk>
2020-05-19 17:29:12 +0100

    conflict code issues again fun
    just code being speicla

--- login.php
+++ login.php
@@ -6,17 +6,17 @@ if(isset($_POST["username"])) {
     $uname= "";
     $pword ="";
 }
- 
-// When the log in as be checked  it will start the session for this page.  
+
+// When the log in as be checked  it will start the session for this page.
 session_start();
 $_SESSION['login'] = "";
- 
+
 // This if statement will only work after a POST happens
 if($_SERVER['REQUEST_METHOD'] === "POST") {
     // Connect to the DB & check for error
     $conn = new mysqli("localhost", "root", "root", "c9");
     if($conn->connect_error) die("[E] Could not connect to DB: " . $conn->connect_error);
-   
+
     // Run the SQL code & check for error
     $query = $conn->query("SELECT * FROM users WHERE Username = '$uname' AND Password = '$pword'");
     $query2 = $conn->query("SELECT * FROM tutor WHERE Username = '$uname' AND Password = '$pword'");
@@ -25,10 +25,10 @@ if($_SERVER['REQUEST_METHOD'] === "POST") {
     // Copy num_rows to result var
     $result = $query->num_rows;
     $results = $query2->num_rows;
-   
+
     // Open error.txt or die if read-only folder
     $error = fopen("error.txt", "a") or die("[E] Cannot open error.txt");
-   
+
     // If there are more than zero rows in result, but less than two (so one) set login to true
     if($result > 0 && $result < 2) {
         $_SESSION['login']= "That GRRRRREAT";
@@ -37,7 +37,7 @@ if($_SERVER['REQUEST_METHOD'] === "POST") {
     } else {
         // Tell the user there is no such username/password
         echo "<div class='WrongData'> The username or password you have entered is not correct.</div>";
-       
+
         // When the user has failed to log in it will write it into the error text file, this is make sure that it will show what the user has tried so it can prevent anyone for guessing the usernames/passwords.
         fwrite ($error, "The username is: " . $_POST['username']. "\nThe password  is: "  .   $_POST['password'] . "\n\n");
     }
@@ -48,7 +48,7 @@ if($_SERVER['REQUEST_METHOD'] === "POST") {
     } else {
         // Tell the user there is no such username/password
         echo "<div class='WrongData'> The username or password you have entered is not correct.</div>";
-       
+
         // When the user has failed to log in it will write it into the error text file, this is make sure that it will show what the user has tried so it can prevent anyone for guessing the usernames/passwords.
         fwrite ($error, "The username is: " . $_POST['username']. "\nThe password  is: "  .   $_POST['password'] . "\n\n");
     }
@@ -58,9 +58,9 @@ if($_SERVER['REQUEST_METHOD'] === "POST") {
 
 // Check the login variable now to see if it is true or not. If it is true, redirect.
 if($_SESSION['login'] == "That GRRRRREAT") header("Location: profilepage.php");
- 
+
 ?>
- 
+
 
 
 <!doctype html>
@@ -88,7 +88,6 @@ if($_SESSION['login'] == "That GRRRRREAT") header("Location: profilepage.php");
         <div class="row">
 
 <div class=" LoginBoxContainer col-xs-12 col-md-6">
-<<<<<<< HEAD
 
  <h3>Existing users  </h3>
 
@@ -117,7 +116,7 @@ if($_SESSION['login'] == "That GRRRRREAT") header("Location: profilepage.php");
              </fieldset>
 
              </form>
- 
+
  </div>
 
 
@@ -133,50 +132,6 @@ if($_SESSION['login'] == "That GRRRRREAT") header("Location: profilepage.php");
 <p> If you are a tutor and don't have a log in please <a href="sign_upTutor.php"> Sign up</a>
 <a class= "btn btn-default btn-primary form-control" href="sign_upTutor.php">Sign up</a>
 </div>
-=======
-
- <h3>Existing users  </h3>
-
-    <form action="login.php" method="post">
-
-        <fieldset>
-
-        <div>
-            <label>Username</label>
-                <input size ="17" type= "text" class="Medium Text form-control" size"20" maxlengh= "15" name="username">
-                </div>
-
-            <div>
-
-           <label> Password </label>
-
-            <input size ="17" type= "password" size"20" maxlengh= "15" name="password" class="Medium Text form-control">
-
-             </div>
-             <div class="next loginbutton">
-
-           <input type="submit" value="login" class="btn btn-primary form-control">
-
-             </div>
-
-             </fieldset>
-
-             </form>
- 
- </div>
-
-
-
-<div class="col-xs-12 col-md-6">
-
-<h3> Not Registered?</h3>
-<p> If  you are a student and don't have a log in please <a href="sign_up.php">Click here</a>     </p>
-<a class= "btn btn-default btn-primary form-control" href="sign_up.php">Sign up</a>
-
-<p> If you are a tutor and don't have a log in please <a href="sign_upTutor.php"> Sign up</a>
-<a class= "btn btn-default btn-primary form-control" href="sign_upTutor.php">Sign up</a>
-
->>>>>>> 60e53da2d963d01990f9348501812428236030e8
     </div>
      </div>
         </div>
@@ -191,4 +146,3 @@ if($_SESSION['login'] == "That GRRRRREAT") header("Location: profilepage.php");
 </body>
 
 </html>
-
--- messages.php
+++ messages.php
@@ -11,18 +11,9 @@ if($_SESSION['login'] != "That GRRRRREAT") {
 <html>
 	<head>
 	<?php require 'meta.php' ?>
-<<<<<<< HEAD
     <?php require 'headerlogin.php'?>
-  
-=======
-<<<<<<< HEAD
-    <?php require 'headerlogin.php'?>
-  
-=======
-  
- 
->>>>>>> 4821221142a69996b6411f3f65680deae984cc36
->>>>>>> 60e53da2d963d01990f9348501812428236030e8
+
+
 		<link rel="stylesheet" type="text/css" href="css/main.css">
 		<meta content="width=device-width, initial-scale=1" name="viewport" />
  <meta charset="utf-8">
@@ -32,18 +23,11 @@ if($_SESSION['login'] != "That GRRRRREAT") {
 
 	</head>
 	<body>
-<<<<<<< HEAD
 
-=======
-<<<<<<< HEAD
- 
-=======
-  <?php require 'headerlogin.php' ?>
->>>>>>> 4821221142a69996b6411f3f65680deae984cc36
->>>>>>> 60e53da2d963d01990f9348501812428236030e8
+
   <?php require 'newmessage.php' ?>
   <?php require 'LeftSide.php' ?>
- 
+
 		<div id="rightside">
 
 	      		<div id="mainmessages">
@@ -65,11 +49,7 @@ if($_SESSION['login'] != "That GRRRRREAT") {
 								{ //if the query is successful then do this.
 									while ($row = mysqli_fetch_assoc($results))
 										{
-<<<<<<< HEAD
-												
-=======
-	  
->>>>>>> 60e53da2d963d01990f9348501812428236030e8
+
 											//$user_sender = $row['messager'];, in the database.
 
 	  // this will be the same through out the whole database..
@@ -80,15 +60,8 @@ if($_SESSION['login'] != "That GRRRRREAT") {
 	  					if($user_sender == $_SESSION['username'])
 	  					{ ?>
 	    				<div class="users">
-<<<<<<< HEAD
 	      				<p> <?php echo $_SESSION['username']; ?></p>
-=======
-<<<<<<< HEAD
-	      				<p> <?php echo $_SESSION['username']; ?></p>
-=======
-	      				<p class="Main"><b> <?php echo $_SESSION['username']; ?></b></p>
->>>>>>> 4821221142a69996b6411f3f65680deae984cc36
->>>>>>> 60e53da2d963d01990f9348501812428236030e8
+
 	      					<p> <?php echo $message ?></p>
 	    						</div>
 	    								<?php
@@ -107,26 +80,15 @@ if($_SESSION['login'] != "That GRRRRREAT") {
 								}
 									}
 	        						?>
-		  </div> 
+		  </div>
 		   <div class="container">
 		   <div class="row">
-<<<<<<< HEAD
-		    
+
 			<div class="col-md-2">
 			</div>
 			<div class=" col-12 col-md-10">
-				<form method="post" id="message-form" >  
+				<form method="post" id="message-form" >
 				<input class="textarea" id="message_text" placeholder= "Write your message"/>
-=======
-		     <?php require 'test.php' ?>
-			<div class="col-md-2">
-			</div>
-			<div class=" col-12 col-md-10">
-				<form method="post" id = "msgbox">
-					<input type= "text" class="minputs" name="nmesss" id = "minputs" placeholder="write the message">
-					<input type="submit" name="submit" value="Send"  class= "btn-primary">
-				</form>
->>>>>>> 60e53da2d963d01990f9348501812428236030e8
 											</div>
 	  			</div>
 							</div>
@@ -134,42 +96,28 @@ if($_SESSION['login'] != "That GRRRRREAT") {
 										 </div>
 							</div>
 </body>
-<<<<<<< HEAD
 <script src="jquery-3.1.1.min.js"> </script>
 <script>
-$("document").ready(function(e) 
+$("document").ready(function(e)
 {
-	//now if the fomr is sumbitted 
+	//now if the fomr is sumbitted
 	$("#message-form").on('submit',  function(){
-		//take the data from input 
+		//take the data from input
    var message_text = $("#message_text").val();
 		//post it to sned.php
-   $.ajax({ 
+   $.ajax({
 	  type:'POST',
 	  url:'send.php?user=<?php echo $_GET['user']?>',
 	  data:{
 	   text:message_text,
-=======
-<script>
-$("document").ready(function(event) 
-{
-	//now if the fomr is sumbitted 
-	$("#rightside").on('submit', 'msg', function(){
-		//take the data from input 
-   var message_text = $("minput").val();
-<<<<<<< HEAD
-		//post it to sned.php
-=======
-		//post it to sned.php 
->>>>>>> 4821221142a69996b6411f3f65680deae984cc36
    $.post("send.php?user=<?php echo$_GET['user']?>",
    { text:message_text,
->>>>>>> 60e53da2d963d01990f9348501812428236030e8
-	
+
+
    },
    success:function(data)
    {
-	   
+
 	   $("#message_text").val(),
 	   document.getElementById("mainmessages").innerHTML += data;
    }
@@ -177,23 +125,16 @@ $("document").ready(function(event)
 	});
 	$("#message-form").keypress(function(e)
 	{
-				 //sumbits when enter is only pressed 
-<<<<<<< HEAD
+				 //sumbits when enter is only pressed
      if(e.keyCode == 13 && !e.shiftKey)
-=======
-     if(e.keyCode ==13 && !e.shiftKey)
->>>>>>> 60e53da2d963d01990f9348501812428236030e8
 	 {
 		 $("#message_text").submit();
-		
+
 
 	 }
 	});
 });
-<<<<<<< HEAD
 
 
-=======
->>>>>>> 60e53da2d963d01990f9348501812428236030e8
 </script>
 </html>
