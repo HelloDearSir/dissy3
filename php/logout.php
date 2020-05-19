2bc1a21df6892445bd6b7898637ec4f9586023ae
HelloDearSir <jonbarton@hotmail.co.uk>
2020-05-19 01:32:34 +0100

    Revert "c"
    This reverts commit 129a2cc56e45b2ede803ed027d9810fc07c8c662.

---
+++ php/logout.php
@@ -0,0 +1,13 @@
+<?php
+
+session_start();
+
+
+//This function will destroy your session
+
+session_destroy();
+
+echo "You are now logged out of your account! <a href=index.php>Home</a> or <a href=../login.php>Login</a>";
+
+
+?>
