9b341c0f11089b5c900f74638386032ff2b2f1a3
HelloDearSir <jonbarton@hotmail.co.uk>
2020-05-22 00:54:40 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function display()
+{
+ document.getElementById('newcontact').style.display='block';
+}
+
+function cancel()
+{
+  document.getElementsByClassName('newcontact').style.display="none";
+}
+
+
