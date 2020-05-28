6bd852d0ba822c6fc767c3fa15a52a29afb61546
HelloDearSir <jonbarton@hotmail.co.uk>
2020-05-28 03:14:58 +0100

    Found my homepage

---
+++ index.php
@@ -0,0 +1,25 @@
+
+
+<!doctype html>
+
+<html>
+
+  <?php require 'meta.php' ?>
+
+    <body>
+
+<?php require 'header.php'  ?>
+
+<div class="container ">
+   <h1 class ="welcome"> Welcome to P2T
+
+    </h1>
+  <p id = "para">
+     if you are new to this application please click on sign up.
+
+    </p>
+      </div>
+
+
+
+</html>
